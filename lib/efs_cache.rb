6371277c441d72725c4d44f251f03f795a35a738
Ryan Jansen <ryanj@openquire.com>
2018-11-06 13:43:40 -0500

    allow mount point owner

--- lib/efs_cache.rb
+++ lib/efs_cache.rb
@@ -16,7 +16,12 @@ module EfsCache
       yield self if block_given?
 
       raise ArgumentError, 'No mount point specified' unless @settings['mount_point']
-      FileUtils.mkdir_p(self.mount_point)
+
+      if !Dir.exist?(self.mount_point)
+        FileUtils.mkdir_p(self.mount_point)
+        dir_owner = self.mount_owner
+        FileUtils.chown(dir_owner, dir_ownerself.mount_point) unless dir_owner.blank?
+      end
     end
 
     def logger
@@ -37,6 +42,14 @@ module EfsCache
 
     def mount_point=(new_mount_point)
       @settings['mount_point'] = new_mount_point
+    end
+
+    def mount_owner
+      @settings['mount_owner']
+    end
+
+    def mount_owner=(new_mount_owner)
+      @settings['mount_owner'] = new_mount_owner unless new_mount_owner.blank?
     end
 
     protected
